d8155b49d1543ddf8b0d4f24d4c964646ff3997f
Hein Htet Zaw <bogeyhz.325@gmail.com>
2023-06-17 16:16:53 +0630

    progress bar finishedd

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Path from "./routes/Path";
-import Sales from "./components/Sales/Sales";
 
 const App = () => {
   return (
     <>
-      <Sales />
-      {/* <Path />     */}
+      <Path />
     </>
   );
 };
