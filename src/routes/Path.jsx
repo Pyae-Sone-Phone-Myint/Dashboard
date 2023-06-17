00c154251e6c3fd0dff65280c5ce60ffa0dda631
Pyae-Sone-Phone-Myint <codewithme201@gmail.com>
2023-06-17 13:57:48 +0630

    login-register

--- src/routes/Path.jsx
+++ src/routes/Path.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Login from "../pages/Login/Login";
+import Register from "../pages/Login/Register";
 
 const Path = () => {
   return (
     <>
-      
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register/>} />
+      </Routes>
     </>
-  )
-}
+  );
+};
 
-export default Path
+export default Path;
