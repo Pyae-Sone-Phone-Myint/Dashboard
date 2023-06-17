48c0c8fd109c2a4b0c5b1f5c21dbf172c145603d
Hein Htet Zaw <bogeyhz.325@gmail.com>
2023-06-17 09:37:30 +0630

    monitor section finished

--- src/routes/Path.jsx
+++ src/routes/Path.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Monitor from '../components/monitor/Monitor'
 
 const Path = () => {
   return (
     <>
-      
+      <BrowserRouter>
+      <Routes>
+        <Route  path='/' element={<Monitor/>}/>
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
