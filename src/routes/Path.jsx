5015266914b59c3ec4e3e7ea5d586fc2658a041b
Aung Ko <aungko@Aungs-MacBook-Pro.local>
2023-06-19 21:12:55 +0700

    charts addeed

--- src/routes/Path.jsx
+++ src/routes/Path.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Chart from '../component/Chart'
+
 
 const Path = () => {
   return (
     <>
+      <Routes>
+        <Route path='/' element={<Chart/>}/>
       
+      </Routes>
     </>
   )
 }
