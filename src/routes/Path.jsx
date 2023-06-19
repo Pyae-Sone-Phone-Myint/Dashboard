ba04b4654b04e2911d96077d4b51bb05d3e4b9b6
Thiha15101051 <at2330227@gmail.com>
2023-06-19 12:17:49 +0630

    contact_table

--- src/routes/Path.jsx
+++ src/routes/Path.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import Contact_table from '../components/contact/Contact_table'
+
 
 const Path = () => {
   return (
     <>
-      
+      <Contact_table/>
     </>
   )
 }
