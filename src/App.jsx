5234f12b13c3f3fbc50ad863ca688b9d37832478
Hein Htet Zaw <bogeyhz.325@gmail.com>
2023-06-19 22:03:54 +0630

    fadf

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Path from "./routes/Path";
+import LatestActivity from "./components/latestActivity/LatestActivity";
 
 const App = () => {
   return (
     <>
-      <Path />
+      <LatestActivity />
+      {/* <Path /> */}
     </>
   );
 };
